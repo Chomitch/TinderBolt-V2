74e628b605e7dc3c8f0a61c0bf879a1aa985ad52
Boris Khomich <chomitch@gmail.com>
2025-05-20 11:33:42 +0300

    day 1

---
+++ bot.py
@@ -0,0 +1,30 @@
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, CallbackQueryHandler, CommandHandler
+
+from gpt import *
+from util import *
+
+# тут будем писать наш код :)
+async def hello(update, context):
+    await send_photo(update, context, "avatar_main")
+    await send_text(update, context, "Привет!")
+    await send_text(update, context, f"Ты написал: \"{update.message.text}\"")
+    await send_text_buttons(update, context, "Выберите режим работы", {  # Текст перед кнопкой
+        "btn_start": " Старт ", # Текст и команда кнопки "Старт"
+        "btn_stop": " Стоп "  # Текст и команда кнопки "Стоп"
+    })
+
+async def hello_button(update, context):
+    query = update.callback_query.data   #код кнопки
+    await update.callback_query.answer() #помечаем что обработали нажатие на кнопку
+    await send_html(update, context, "Вы нажали на кнопку " + query)
+
+async def start(update, context):
+    await send_photo(update, context, "main")
+    await send_text(update, context, load_message("main"))
+
+
+app = ApplicationBuilder().token("7854383222:AAEIJ5Rxt-SgOQkMLuFrQr_RAHtbQosz-Gk").build()
+app.add_handler(CommandHandler("start", start))
+app.add_handler(MessageHandler(filters.TEXT&~filters.COMMAND, hello))
+app.add_handler(CallbackQueryHandler(hello_button))
+app.run_polling()
